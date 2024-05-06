1ebd567bde82fd8eb132a5b65e9962bd5acbdb30
turboJavaEnjoyer <126062651+turboJavaEnjoyer@users.noreply.github.com>
2024-05-06 16:44:26 +0200

    Borrado curl_alumno_por_dni.sh

--- curl_alumno_por_dni.sh
+++
@@ -1,4 +0,0 @@
-#Obtener alumno especificando el DNI por la linea de comandos
-KEY=$(curl -s -X POST "http://localhost:9090/CentroEducativo/login" -H  "accept: text/>
-alumno=$(curl -s -X GET "http://localhost:9090/CentroEducativo/alumnos/"$1"/?key="$KEY>
-echo $alumno
