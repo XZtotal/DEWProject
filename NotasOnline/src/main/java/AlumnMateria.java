3a77f9a2f96538b17b648bf450db956c3e11e8bd
atorube@alumno.upv.es <atorube@alumno.upv.es@ldsic-vdi06.cc.upv.es>
2024-05-22 15:34:23 +0200

    Pagina de retorno html materia alumno

---
+++ NotasOnline/src/main/java/AlumnMateria.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import utils.LogUtil;
+
+/**
+ * Servlet implementation class Logout
+ */
+public class AlumnMateria extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AlumnMateria() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Invalidar la sesión
+        HttpSession session = request.getSession(false); // Obtener la sesión actual, si existe
+        if (session != null) {
+            session.invalidate();
+        }
+        
+        // Realizar logout
+        LogUtil.log(request,"logout");
+        request.logout();
+        
+        // Redirigir a la página de inicio o login
+        response.sendRedirect(request.getContextPath() + "/index.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
