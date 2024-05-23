730730c3b57234d9b8767fbf2ead2002b3e94cda
DavidGalRiv <130923280+DavidGalRiv@users.noreply.github.com>
2024-05-23 18:01:43 +0200

    Update AlumnService.java
    sendRequest se quita de aqui ya que se hace un import util.Utils

--- NotasOnline/src/main/java/AlumnService.java
+++ NotasOnline/src/main/java/AlumnService.java
@@ -15,6 +15,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import utils.Utils;
+
 
 /**
  * Servlet implementation class AlumnService
@@ -69,23 +71,5 @@ public class AlumnService extends HttpServlet {
 		doGet(request, response);	
 
 
-	private String sendRequest(String url, String method, JSONObject body) throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-                .uri(new URI(url));
 
-        if ("POST".equalsIgnoreCase(method) || "PUT".equalsIgnoreCase(method)) {
-            requestBuilder.header("Content-Type", "application/json");
-            requestBuilder.method(method, HttpRequest.BodyPublishers.ofString(body.toString()));
-        } else {
-            requestBuilder.method(method, HttpRequest.BodyPublishers.noBody());
-        }
-
-        HttpRequest request = requestBuilder.build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() != 200 && response.statusCode() != 201 && response.statusCode() != 204) {
-            throw new RuntimeException("Error en la petición: " + response.statusCode());
-        }
-        return response.body();
-    }
 }
